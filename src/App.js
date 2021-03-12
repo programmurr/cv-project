99ed16c28ce02df0d9455c3670c80743c3bf8fc4
conor <conorjm89@gmail.com>
2021-03-12 23:27:48 +0000

    Complete refactoring to functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import GeneralInfo from './components/GeneralInfo'
 import EducationDisplay from './components/EducationDisplay'
@@ -7,138 +7,91 @@ import PracticalExperienceDisplay from './components/PracticalExperienceDisplay'
 import PracticalExperienceForm from './components/PracticalExperienceForm'
 
 
-class App extends React.Component {
-  constructor() {
-    super();
+function App() {
+  const [ generalInfo, setGeneralInfo ] = useState([]);
+  const [ infoSubmitted, setInfoSubmitted ] = useState(false);
+  const [ educationInfo, setEducationInfo ] = useState([]);
+  const [ experienceInfo, setExperienceInfo ] = useState([]);
 
-    this.state = {
-      generalInfo: [],
-      infoSubmitted: false, 
-      educationInfo: [],
-      experienceInfo: []
-    }
-
-    this.handleInfoSubmit = this.handleInfoSubmit.bind(this);
-
-    this.handleEducationSubmit = this.handleEducationSubmit.bind(this);
-    this.handleEducationEdit = this.handleEducationEdit.bind(this);
-    this.handleEducationDelete = this.handleEducationDelete.bind(this);
-
-    this.handleExperienceSubmit = this.handleExperienceSubmit.bind(this);
-    this.handleExperienceEdit = this.handleExperienceEdit.bind(this);
-    this.handleExperienceDelete = this.handleExperienceDelete.bind(this);
+  function handleInfoSubmit(info, status) {
+    setGeneralInfo([info]);
+    setInfoSubmitted(status);
   }
 
-  handleInfoSubmit(info, status) {
-    this.setState({
-      generalInfo: [info],
-      infoSubmitted: status
-    });
+  function handleEducationSubmit(info) {
+    const newEducationInfo = educationInfo.concat(info);
+    setEducationInfo(newEducationInfo);
   }
 
-  handleEducationSubmit(info) {
-    const state = this.state;
-
-    this.setState({
-      educationInfo: state.educationInfo.concat(info),
-    });
+  function handleEducationEdit(info) {
+    const index = educationInfo.findIndex((edu => edu.id === info.id));
+    educationInfo[index] = info;
+    setEducationInfo([...educationInfo]);
   }
 
-  handleEducationEdit(info) {
-    const state = Object.assign({}, this.state);
-    const index = state.educationInfo.findIndex((edu => edu.id === info.id));
-    state.educationInfo[index] = info;
-    // state.educationInfo[index].editClicked = false;
-
-    this.setState({ state })
+  function handleEducationDelete(info) {
+    const newEducationInfo = educationInfo.filter((edu => edu.id !== info.id))
+    setEducationInfo(newEducationInfo);
   }
 
-  handleEducationDelete(info) {
-    const state = Object.assign({}, this.state);
-    const newEducationInfo = state.educationInfo.filter((edu => edu.id !== info.id));
-    
-    this.setState({
-      educationInfo: newEducationInfo,
-    })
+  function handleExperienceSubmit(info) {
+    const newExperienceInfo = experienceInfo.concat(info);
+    setExperienceInfo(newExperienceInfo);
   }
 
-  handleExperienceSubmit(info) {
-    const state = this.state;
-
-    this.setState({
-      experienceInfo: state.experienceInfo.concat(info)
-    });
+  function handleExperienceEdit(info) {
+    const index = experienceInfo.findIndex((exp => exp.id === info.id));
+    experienceInfo[index] = info;
+    setExperienceInfo([...experienceInfo]);
   }
 
-  handleExperienceEdit(info) {
-    const state = Object.assign({}, this.state);
-    const index = state.experienceInfo.findIndex((exp => exp.id === info.id));
-    state.experienceInfo[index] = info;
-    // state.experienceInfo[index].editClicked = false;
-
-    this.setState({ state })
+  function handleExperienceDelete(info) {
+    const newExperienceInfo = experienceInfo.filter((exp => exp.id !== info.id));
+    setExperienceInfo(newExperienceInfo);
   }
 
-  handleExperienceDelete(info) {
-    const state = Object.assign({}, this.state);
-    const newExperienceInfo = state.experienceInfo.filter((exp => exp.id !== info.id));
-
-    this.setState({
-      experienceInfo: newExperienceInfo
-    })
-  }
-
-  render() {
-    const { 
-      generalInfo, 
-      infoSubmitted,
-      educationInfo,
-      experienceInfo,
-    } = this.state;
-
-    return (
-      <div className="container">
-        <div className="containerHeader">
-          <h1>CV Builder</h1>
-        </div>
-        <GeneralInfo 
-          info={generalInfo} 
-          onInfoSubmit={this.handleInfoSubmit}
-          infoSubmitted={infoSubmitted}
-        />
-        <h2>Education</h2>
-        {educationInfo.length > 0 
-          ? educationInfo.map(education => (
-              <EducationDisplay
-                key={education.id}
-                educationInfo={education}
-                onEducationEdit={this.handleEducationEdit}
-                onEducationDelete={this.handleEducationDelete}
-              />
-          ))
-          : <p>Enter your education details below!</p>
-          }
-        <EducationForm 
-          onEducationSubmit={this.handleEducationSubmit} 
-        />
-        <h2>Practical Experience</h2>
-        {experienceInfo.length > 0
-          ? experienceInfo.map(experience => (
-              <PracticalExperienceDisplay
-                key={experience.id}
-                experienceInfo={experience}
-                onExperienceEdit={this.handleExperienceEdit}
-                onExperienceDelete={this.handleExperienceDelete}
-              />
-          ))
-          : <p>Enter your practical experience below!</p>
-          }
-        <PracticalExperienceForm 
-          onExperienceSubmit={this.handleExperienceSubmit} 
-        />
+  return (
+    <div className="container">
+      <div className="containerHeader">
+        <h1>CV Builder</h1>
       </div>
-    )
-  }
+      <GeneralInfo 
+        info={generalInfo} 
+        onInfoSubmit={handleInfoSubmit}
+        infoSubmitted={infoSubmitted}
+      />
+      <h2>Education</h2>
+      {educationInfo.length > 0 
+        ? educationInfo.map(education => (
+            <EducationDisplay
+              key={education.id}
+              educationInfo={education}
+              onEducationEdit={handleEducationEdit}
+              onEducationDelete={handleEducationDelete}
+            />
+        ))
+        : <p>Enter your education details below!</p>
+        }
+      <EducationForm 
+        onEducationSubmit={handleEducationSubmit} 
+      />
+      <h2>Practical Experience</h2>
+      {experienceInfo.length > 0
+        ? experienceInfo.map(experience => (
+            <PracticalExperienceDisplay
+              key={experience.id}
+              experienceInfo={experience}
+              onExperienceEdit={handleExperienceEdit}
+              onExperienceDelete={handleExperienceDelete}
+            />
+        ))
+        : <p>Enter your practical experience below!</p>
+        }
+      <PracticalExperienceForm 
+        onExperienceSubmit={handleExperienceSubmit} 
+      />
+    </div>
+  )
 }
 
 export default App;
